eb54587e2201eab2dc0cab31fa6b0521be7d66f7
Peter Christman <peter.christman@gmail.com>
2014-11-10 19:01:59 -0600

    update1

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,26 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @zebra = Photo.find(params[:id])
+  end
+
+  def new_form
+
+  end
+
+  def create_row
+    @the_caption = params[:the_caption]
+    @the_source = params[:the_source]
+  end
+
+  def destroy
+    @zebra = Photo.find(params[:id])
+    @zebra.destroy
+  end
+
+  def edit_form
+  end
+
 
 end
